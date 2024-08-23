bcb9845af4c4b5bc68b6d7acf69eafaa58637661
karusaini <karinasaini06@gmail.com>
2024-08-23 15:12:04 +0530

    done caching

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,34 +1,36 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import prisma from "@/app/lib/db"
+import prisma from "@/app/lib/db";
 import { NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET() {
-          const {getUser} = getKindeServerSession()
+  noStore();
+  const { getUser } = getKindeServerSession();
 
-          const user = await getUser()
+  const user = await getUser();
 
-          if (!user || user === null || !user.id) {
-                    throw new Error("Something went wrong, i am sorry...")
-          }
+  if (!user || user === null || !user.id) {
+    throw new Error("Smoething went wrong, i am srorry....");
+  }
 
-          let dbUser = await prisma.user.findUnique({
-                    where: {
-                              id: user.id,
-                    }
-          })
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
 
-          if (!dbUser) {
-                    dbUser = await prisma.user.create({
-                              data: {
-                                        email:user.email ?? "",
-                                        firstName: user.given_name ?? "",
-                                        lastName: user.family_name ?? "",
-                                        id: user.id
-,
-profileImage: user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,                              
-                              }
-                    })
-          }
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        email: user.email ?? "",
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        id: user.id,
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+      },
+    });
+  }
 
-          return NextResponse.redirect("http://localhost:3000")
+  return NextResponse.redirect("https://localhost:3000/");
 }
